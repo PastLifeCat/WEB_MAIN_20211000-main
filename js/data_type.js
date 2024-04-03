8ebd3f9f59ffa1b9723846a7b8acfc63fcdafd8a
admin <yjj2486123@gmail.com>
2024-04-03 13:45:27 +0900

    04.03 수업

--- js/data_type.js
+++ js/data_type.js
@@ -21,3 +21,24 @@ isMale: true,
 };
 console.log(symbolVar1.toString()); // 문자열 변환 출력
 console.log(obj1, obj2, airline); // 여러 개 출력
+
+const users = new Map(); // 사용자 정보 Map 객체 생성
+users.set("user1", { // 사용자 정보 추가
+id: 1, password: "password123",
+});
+users.set("user2", {
+id: 2, password: "password456",
+});
+// Map 객체의 모든 사용자 정보 반복 출력
+for (const [username, user] of users) {
+console.log(`사용자 이름: ${username}`, `ID: ${user.id}`);
+console.log(`비밀번호: ${user.password}`);
+}
+// Set 객체 활용 (예), 이름만 저장할 Set 객체 생성
+const usernames = new Set();
+usernames.add("user1"); // 사용자 이름 추가
+usernames.add("user2");
+// Set 객체의 모든 사용자 이름 반복 출력
+for (const username of usernames) {
+console.log(`사용자 이름: ${username}`);
+}
--- js/search.js
+++ js/search.js
@@ -2,3 +2,19 @@ document.getElementById("search_btn").addEventListener('click', search_message);
 function search_message(){
 alert("검색을 수행합니다!");
 }
+
+function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+    // 새 창에서 구글 검색을 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+    return false;
+    }
+    
+/*function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    const googleSearchUrl = `https://www.op.gg/summoners/kr/${encodeURIComponent(searchTerm)}`;
+    // 새 창에서 구글 검색을 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+    return false;
+    } */    //op.gg 검색 (그냥 만들어봄)
