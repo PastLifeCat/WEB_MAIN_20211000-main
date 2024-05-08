521f0e1ed329b3851ba83478402b4d8e8a301cfc
admin <yjj2486123@gmail.com>
2024-05-08 14:18:17 +0900

    05.08.수업

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+const idsave_check = document.getElementById('idSaveCheck');
 const check_xss = (input) => {
     // DOMPurify 라이브러리 로드 (CDN 사용)
     const DOMPurify = window.DOMPurify;
@@ -72,8 +73,37 @@ const check_input = () => {
 
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
+    if(idsave_check.checked == true) {//아이디 체크O
+        alert("쿠키를 저장합니다.",emailValue);
+        setCookie("id", emailValue, 1); //1일 저장
+        alert("쿠키 값 :" + emailValue);
+    }
+    else{ //아이디 체크 X
+        setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
+    }
     loginForm.submit();
     };
+
+    function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
+        }
+    
+    function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+        var cookie_array = cookie.split("; ");
+        for ( var index in cookie_array) {
+        var cookie_name = cookie_array[index].split("=");
+        if (cookie_name[0] == "id") {
+        return cookie_name[1];
+        }
+        }
+        }
+        return ;
+        }
     
     document.getElementById("login_btn").addEventListener('click', check_input);
 
