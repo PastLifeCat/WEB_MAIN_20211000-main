554a7cd0b20712f4d919730524f0aaa4efce5a05
admin <yjj2486123@gmail.com>
2024-05-22 14:43:23 +0900

    05.22.수업

--- js/logout.js
+++ js/logout.js
@@ -5,13 +5,13 @@ function logout()
     session_del(); // 세션 삭제
     location.href='../index.html'
 }
-document.getElementById("logout_btn").addEventListener('click', check_input);
+document.getElementById("logout_btn").addEventListener('click', logout);
 
 function session_del() 
 {//세션 삭제
     if (sessionStorage) 
         {
-        sessionStorage.removeItem("Session_Storage_test");
+        sessionStorage.removeItem("Session_Storage_pass");
         alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
         } 
         else 
