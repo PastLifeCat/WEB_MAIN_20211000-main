6c9a424c947849f87f028e40a0aeee66a9e47df2
admin <yjj2486123@gmail.com>
2024-06-11 02:23:45 +0900

    06.11 수정

---
+++ js/join.js
@@ -0,0 +1,22 @@
+function join()
+    { // 회원가입
+    let form = document.querySelector("#form_main");
+    let f_name = document.querySelector("#firstName");
+    let l_name = document.querySelector("#lastName");
+    let b_day = document.querySelector("#birthdayDate");
+    let gender = document.querySelector("#inlineRadioOptions");
+    let email = document.querySelector("#emailAddress");
+    let p_number = document.querySelector("#phoneNumber");
+    let class_check = document.querySelector(".select form-control-lg");
+
+    form.action = "../login/index_join.html";
+    form.method = "get";
+
+    if(f_name.value.length === 0 || l_name.value.length === 0 || b_day.value.length === 0 || email.value.length === 0 || p_number.value.length === 0)
+        {
+        alert("회원가입 폼에 필수 정보를 입력해주세요.(성별, 분반 제외)");
+        }
+        else{
+            form.submit();
+            }
+    }
