daa8f0706bdcc91dc1ce01216816f56917c2ea62
admin <yjj2486123@gmail.com>
2024-06-11 02:49:01 +0900

    06.11 수정

--- js/join.js
+++ js/join.js
@@ -1,3 +1,15 @@
+function addJavascript(jsname)
+{
+    //자바스크립트 외부 연동
+    var th = document.getElementsByTagName('head')[0];
+    var s = document.createElement('script');
+    s.setAttribute('type', 'text/javascript');
+    s.setAttribute('src', jsname);
+    th.appendChild(s);
+}
+addJavascript('/js/security.js');
+addJavascript('/js/session.js');
+
 function join()
     { // 회원가입
     let form = document.querySelector("#form_main");
@@ -9,7 +21,7 @@ function join()
     let p_number = document.querySelector("#phoneNumber");
     let class_check = document.querySelector(".select form-control-lg");
 
-    form.action = "../login/index_join.html";
+    form.action = "../login/join.html";
     form.method = "get";
 
     if(f_name.value.length === 0 || l_name.value.length === 0 || b_day.value.length === 0 || email.value.length === 0 || p_number.value.length === 0)
@@ -17,6 +29,41 @@ function join()
         alert("회원가입 폼에 필수 정보를 입력해주세요.(성별, 분반 제외)");
         }
         else{
+            session_join_set(); // 회원가입 용 세션 생성
             form.submit();
             }
     }
+
+class SignUp {
+    constructor(firstName, lastName, birthdayDate, gender, emailAddress, phoneNumber, classNumber, random) { // 생성자 함수
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.birthdayDate = birthdayDate;
+      this.gender = gender;
+      this.emailAddress = emailAddress;
+      this.phoneNumber = phoneNumber;
+      this.classNumber = classNumber;
+      this.random = random;
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`; // 템플릿 리터럴 문자열 연결, 기존에는 + 연산자로 연결
+      }
+    
+      set fullName(fullName) {
+        const [firstName, lastName] = fullName.split(" ");
+        this.firstName = firstName;
+        this.lastName = lastName;
+      }
+    
+      get contactInfo() {
+        return `${this.emailAddress} ${this.phoneNumber} ${this.random}`; // 요소 하나 하나를 객체 프로퍼티라고 한다.
+      }
+    
+      set contactInfo(contactInfo) {
+        const [emailAddress, phoneNumber, random] = contactInfo.split(" ");
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.random = random;
+      }
+    }
+      
